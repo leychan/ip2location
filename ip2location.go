0dddf5aa5962c828d90dd9cb14455f17bed2df15
lei <sanyue0315@outlook.com>
2024-04-25 14:37:52 +0800

    first commit

---
+++ ip2location.go
@@ -0,0 +1,95 @@
+package ip
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/ip2location/ip2location-go/v9"
+	"github.com/lionsoul2014/ip2region/binding/golang/xdb"
+)
+
+var ip2locationDB *ip2location.DB
+var initIp2locationDbOnce sync.Once
+
+var ip2regionSeacher *xdb.Searcher
+var initIp2regionSeacherOnce sync.Once
+
+var initError error
+
+func init() {
+    initIp2locationDbOnce.Do(func() {
+        ip2locationDB, initError = ip2location.OpenDB("db/lite-9.bin")
+    })
+
+    if initError != nil {
+        panic(initError)
+    }
+    initIp2regionSeacherOnce.Do(func() {
+        cBuffer, _ = xdb.LoadContentFromFile("db/ip2region.xdb")
+	    ip2regionSeacher, initError = xdb.NewWithBuffer(cBuffer)
+    })
+
+    if initError != nil {
+        panic(initError)
+    }
+
+}
+
+func GetIpInfoInternational(ip string) (IPInfo, error) {
+    record, err := ip2locationDB.Get_all(ip)
+    if err != nil {
+        return IPInfo{}, err
+    }
+    return IPInfo{
+        Country: record.Country_short,
+        Region:  record.Region,
+        Area:    "",
+        City:    record.City,
+        Isp:     "",
+    }, nil
+}
+
+
+type IPInfo struct {
+    Country string
+    Region  string
+    Area    string
+    City    string
+    Isp     string
+}
+
+func GetIpInfo(ip string) IPInfo {
+    var ipInfo IPInfo
+	res, err := TransIP2RegionStrOffline(ip)
+	if err != nil {
+		return ipInfo
+	}
+
+	arr := strings.Split(res, "|")
+	if len(arr) < 5 {
+		return ipInfo
+	}
+	//中国|0|北京|北京市|电信
+	ipInfo = IPInfo{
+		Country: arr[0], 
+		Region:  arr[2],
+		Area:    "",
+		City:    arr[3],
+		Isp:     arr[4],
+	}	
+
+	return ipInfo
+}
+
+var cBuffer []byte
+
+
+func TransIP2RegionStrOffline(ip string) (string, error) {
+	defer ip2regionSeacher.Close()
+	//国家|区域|省份|城市|ISP
+	res, err := ip2regionSeacher.SearchByStr(ip)
+	if err != nil {
+		return "", err
+	}
+	return res, nil
+}
---
+++ ip2location_test.go
@@ -0,0 +1,51 @@
+package ip
+
+import (
+	"fmt"
+	"testing"
+)
+
+var ips = []string{
+	"1.202.118.195",
+	"111.55.11.252",
+	"61.242.134.22",
+	"120.197.61.20",
+	"139.210.169.82",
+	"117.61.86.188",
+	"117.61.86.188",
+	"39.144.109.62",
+	"1.80.216.224",
+	"117.136.74.235",
+	"39.149.31.75",
+	"39.149.31.75",
+	"183.15.205.122",
+	"183.15.205.122",
+	"223.104.66.249",
+	"171.81.215.125",
+	"117.172.1.110",
+	"14.151.33.8",
+	"14.151.33.8",
+	"171.43.29.150",
+	"223.104.72.222",
+	"223.104.72.149",
+	"39.144.69.102",
+	"223.104.199.42",
+}
+
+func BenchmarkGetIpInfo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetIpInfo(ips[i%len(ips)])
+	}
+}
+
+func TestGetIpInfo(t *testing.T) {
+	fmt.Println(GetIpInfo("62.210.214.190"))
+	fmt.Println(GetIpInfo("1.202.118.195"))
+	fmt.Println(GetIpInfo("89.185.30.208"))
+}
+
+func TestGetIpInfoInternational(t *testing.T) {
+	fmt.Println(GetIpInfoInternational("62.210.214.190"))
+	fmt.Println(GetIpInfoInternational("1.202.118.195"))
+	fmt.Println(GetIpInfoInternational("89.185.30.208"))
+}
